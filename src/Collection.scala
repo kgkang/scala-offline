9784ae344673094480577e2b381e3a391109309e
KyongGu Kang <kgkang@gmail.com>
2016-10-15 17:39:21 +0900

    type mismatch error fixed

--- src/Collection.scala
+++ src/Collection.scala
@@ -1,7 +1,7 @@
 
 
 object Collection {
-	def values(fun:(Int)=>Int,low:Int,high:Int):Int = {
+	def values(fun:(Int)=>Int,low:Int,high:Int) = {
 		(low to high) map { x => (x, fun(x))}
 	}
 	def main(args: Array[String]): Unit = {
